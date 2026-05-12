60b7a4a87482db0792a698ec03e21c8375c76bc5
chen-zhanjie <1579282698@qq.com>
2026-05-12 11:44:49 +0800

    refactor: consume web core crud request helpers

--- web/src/core/crud/request.ts
+++ web/src/core/crud/request.ts
@@ -1,12 +1,2 @@
-import { serializeCrudParams } from '@trueadmin/web-core/crud';
-import type { CrudListParams } from './types';
-
-type QueryParams = CrudListParams | Record<string, unknown>;
-
-export const toCrudRequestParams = (params?: QueryParams) =>
-  serializeCrudParams(params as CrudListParams | undefined).toString();
-
-export const crudRequestOptions = (params?: QueryParams) => {
-  const query = toCrudRequestParams(params);
-  return query ? { params: query } : undefined;
-};
+export type { CrudRequestOptions, CrudRequestQueryParams } from '@trueadmin/web-core/crud';
+export { crudRequestOptions, toCrudRequestParams } from '@trueadmin/web-core/crud';
